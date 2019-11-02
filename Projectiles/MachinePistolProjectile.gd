2adba59a1f22adba4d97bd549b326d3d96fa8c18
earthcrafterman <banddstudios@gmail.com>
2019-11-01 22:33:15 -0400

    hotfix: fixed misnamed variable

--- Projectiles/MachinePistolProjectile.gd
+++ Projectiles/MachinePistolProjectile.gd
@@ -6,6 +6,6 @@ func on_spawn():
 	fCrit = 35.0
 	fRadius = 0.75
 	fAccelerateDuration = 1.0
-	fAcceleration = -Constants.fPlayerMaxSpeed / fAccelerateDuration
+	fAcceleration = -Constants.fProjectileSpeedMultiplier / fAccelerateDuration
 	fAccelerateTime = 0.0
 	after_spawn()
